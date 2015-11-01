feddeca98a08b7e84c3a5046f99d602deb021147
Cédric Luthi <cedric.luthi@gmail.com>
2015-11-01 13:58:18 +0100

    Fix `GTLYouTubeResourceId` allocation
    Code that [allocates `GTLObject` instances](https://github.com/google/google-api-objectivec-client/blob/4e1c06132cc88ff81f31e280dc99a38a769603e0/Source/Objects/GTLObject.m#L554-L559) uses the `kind` key to determine which subclass should be allocated. This works well except for `GTLYouTubeResourceId` where the `kind` key doesn’t have the same meaning.
    
    The following JSON excerpt allocates a `GTLYouTubeVideo` object instead of a `GTLYouTubeResourceId` object:
    
    ```json
    "resourceId" : {
        "kind" : "youtube#video",
        "videoId" : "RjtCS0EEoCY"
    }
    ```
    
    Of course using something like `playlistItem.snippet.resourceId.videoId` crashes with the following exception: `-[GTLYouTubeVideo videoId]: unrecognized selector sent to instance 0x7fbce9545330`
    
    This commit addresses this issue by ignoring the `kind` key for *resource id* objects.

--- Source/Objects/GTLObject.m
+++ Source/Objects/GTLObject.m
@@ -552,7 +552,7 @@ static NSMutableDictionary *gKindMap = nil;
   NSString *kind = nil;
   if ([json isKindOfClass:[NSDictionary class]]) {
     kind = [json valueForKey:@"kind"];
-    if ([kind isKindOfClass:[NSString class]] && [kind length] > 0) {
+    if ([kind isKindOfClass:[NSString class]] && [kind length] > 0 && ![NSStringFromClass(defaultClass) hasSuffix:@"ResourceId"]) {
       Class dynamicClass = [GTLObject registeredObjectClassForKind:kind];
       if (dynamicClass) {
         classToCreate = dynamicClass;
